0bae5ef7e950bcb4111cb3aac0773c5b0e1d64c3
Nalendraa <shawnaxcel42@gmail.com>
2021-03-22 11:37:52 +0700

    Create Araay1.java

---
+++ Araay1/src/araay1/Araay1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package araay1;
+
+/**
+ *
+ * @author Ali Imron Saefullah E41202285
+ */
+public class Araay1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int [] angka = {0, 1, 3, 4, 8, 11, 13, 23};
+        
+        System.out.println("Total Elemen Array Berjumlah = "+angka.length);
+    }
+    
+}
