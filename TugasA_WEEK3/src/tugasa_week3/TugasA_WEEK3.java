56f249026696fc9371e8fbbc2d099c64d3f5cd3e
Nalendraa <shawnaxcel42@gmail.com>
2021-03-22 14:17:44 +0700

    Create TugasA_WEEK3.java

---
+++ TugasA_WEEK3/src/tugasa_week3/TugasA_WEEK3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasa_week3;
+
+/**
+ *
+ * @author Ali Imron SaefullahE41202285 - WEEK 3 Array 1Dimensi
+ */
+
+import java.util.Scanner;
+
+public class TugasA_WEEK3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String [] nama = {"Sugito", "Prayoga", "Rahardika"};
+        
+        for (int j=0; j<nama.length; j++){
+            System.out.print (nama[j]);
+            System.out.print (" ");
+        }
+        
+    }
+    
+}
